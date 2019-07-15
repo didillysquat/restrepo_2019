815b04935bd84ff45bf8e09e97b5de4e922cb878
didillysquat <didillysquat@gmail.com>
2019-07-15 10:50:17 +0300

    Add Durusdinium to the R PERMANOVAs

--- restrepo_r_code.R
+++ restrepo_r_code.R
@@ -1,9 +1,9 @@
 library(vegan)
-output_path = '/Users/humebc/Google_Drive/projects/alejandro_et_al_2018/restrepo_git_repo/outputs/'
-setwd(output_path)
-dis_matrix = as.dist(data.matrix(read.table(file="dists_A.csv", header=FALSE, sep=',', stringsAsFactors = FALSE)))
-meta_info = read.table(file='meta_info_A.csv', header=TRUE, sep=',')
-adonis(formula = dis_matrix ~ reef*reef_type*depth*season*species, data=meta_info)
+# output_path = '/Users/humebc/Google_Drive/projects/alejandro_et_al_2018/restrepo_git_repo/outputs/'
+# setwd(output_path)
+# dis_matrix = as.dist(data.matrix(read.table(file="dists_A.csv", header=FALSE, sep=',', stringsAsFactors = FALSE)))
+# meta_info = read.table(file='meta_info_A.csv', header=TRUE, sep=',')
+# adonis(formula = dis_matrix ~ reef*reef_type*depth*season*species, data=meta_info)
 
 # SAMPLE PERMANOVAs and tests of heterogeneity of dispersion using PERMDISP2
 # Clade A
@@ -97,5 +97,36 @@ meta_info = read.table(file='sample_meta_info_D.csv', header=TRUE, sep=',')
 adonis(formula = dis_matrix ~ species*reef*reef_type*depth*season, data=meta_info)
 
 # Tests of heterogeneity of dispersion using PERMDISP2
+# Species
+mod_species = betadisper(dis_matrix, meta_info$species)
+anova(mod_species)
+(mod_species.HSD = TukeyHSD(mod_species))
+plot(mod_species, segments = FALSE, ellipse=TRUE)
 
+# Season
+mod_season = betadisper(dis_matrix, meta_info$season)
+anova(mod_season)
+(mod_season.HSD = TukeyHSD(mod_season))
+plot(mod_season, segments = FALSE, ellipse=TRUE)
+
+# Depth
+mod_depth = betadisper(dis_matrix, meta_info$depth)
+anova(mod_depth)
+(mod_depth.HSD = TukeyHSD(mod_depth))
+permutest(mod_depth, pairwise=TRUE)
+plot(mod_depth, segments = FALSE, col = c('cyan', 'blue', 'blue4'), ellipse=TRUE)
+
+# Reef_type
+mod_reef_type = betadisper(dis_matrix, meta_info$reef_type)
+anova(mod_reef_type)
+(mod_reef_type.HSD = TukeyHSD(mod_reef_type))
+permutest(mod_reef_type, pairwise=TRUE)
+plot(mod_reef_type, segments = FALSE, ellipse=TRUE)
+
+# Reef
+mod_reef = betadisper(dis_matrix, meta_info$reef)
+anova(mod_reef)
+(mod_reef.HSD = TukeyHSD(mod_reef))
+permutest(mod_reef, pairwise=TRUE)
+plot(mod_reef, segments = FALSE, ellipse=TRUE)
 
